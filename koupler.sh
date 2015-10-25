8ce59f07fe0c70a070fed9d7adb7aa726b008cb4
Brian O'Neill <boneill@monetate.com>
2015-10-25 07:31:16 -0400

    Fixes #2 Switches build system over to gradle (FTW!)

--- koupler.sh
+++ koupler.sh
@@ -1,1 +1,12 @@
-java -Dlog4j.configurationFile=conf/log4j2.xml -jar target/koupler-0.1.0-SNAPSHOT-jar-with-dependencies.jar $@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+
+CLASSPATH=
+for i in `ls ./build/libs/*.jar`
+do
+  CLASSPATH=${CLASSPATH}:${i}
+done
+
+java -Dlog4j.configurationFile=conf/log4j2.xml -cp ".:${CLASSPATH}" com.monetate.koupler.Koupler
